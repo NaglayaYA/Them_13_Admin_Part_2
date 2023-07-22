563ef7d18c1b2f55a9551e1b1377a4b75804ff29
NaglayaYA <krishjk64@gmail.com>
2023-07-22 14:01:23 +0300

    Them 6 frontend

--- Them5ex1/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Perspective_Them5ex1',
-    __DIR__
-);
--- Them5ex2/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Perspective_Them5ex2',
-    __DIR__
-);
